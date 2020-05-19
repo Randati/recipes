2c11482c4386f2c89fbb592216a5b7a585b315dd
Randati <anttivan@gmail.com>
2020-05-19 19:17:36 +0300

    Add manifest.json

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,7 @@ const CACHE_NAME = 'offline-cache-{{'now' | date: '%s' }}';
 
 const FILES_TO_CACHE = [
 	'/',
-	// '/main.css',
+	'/favicon.png',
 	'/poppins-700.subset.woff2',
 	{%- for recipe in site.recipes %}
 	'{{ recipe.url | relative_url }}',
