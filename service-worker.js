229f201d191cff942f6e74f77480eceac47e756a
Randati <anttivan@gmail.com>
2020-05-21 14:06:43 +0300

    Change icon

--- service-worker.js
+++ service-worker.js
@@ -8,7 +8,7 @@ const CACHE_NAME = 'offline-cache-{{'now' | date: '%s' }}';
 const FILES_TO_CACHE = [
 	'/',
 	'/favicon.png',
-	'/ios-icon.png',
+	'/icon-512.png',
 	'/poppins-700.subset.woff2',
 	{%- for recipe in site.recipes %}
 	'{{ recipe.url | relative_url }}',
